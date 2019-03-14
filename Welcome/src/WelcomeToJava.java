4a02ccd46df97e9b753f541bdff76e22fad86c9d
soulstartlight <sdembele@spscc.edu>
2019-03-14 03:06:18 -0700

    Welcome
    Welcome

---
+++ Welcome/src/WelcomeToJava.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author S SOCIETY
+ *
+ */
+public class WelcomeToJava {
+	public static void main(String[] args) {
+		System.out.println("Welcome to our first java tutorial");
+	}
+
+}
